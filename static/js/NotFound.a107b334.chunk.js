548e35d51dc9c70fbcda5760e573ce0f684c25d0
Ivan <vanya1254@yandex.ru>
2024-01-29 14:58:18 +0300

    Updates

---
+++ static/js/NotFound.a107b334.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_jkpizza=self.webpackChunkreact_jkpizza||[]).push([[187],{4949:(a,e,s)=>{s.r(e),s.d(e,{default:()=>r});s(2791);var c=s(1819),k=s(184);const r=()=>(0,k.jsx)(c.vr,{})}}]);
+//# sourceMappingURL=NotFound.a107b334.chunk.js.map
